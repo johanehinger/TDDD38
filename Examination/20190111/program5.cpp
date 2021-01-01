8fe42b72dba7ed5ea11997a207d4768b0e2956ff
Johan Ehinger <joheh148@student.liu.se>
2021-01-01 18:51:15 +0100

    program5 done

--- Examination/20190111/program5.cpp
+++ Examination/20190111/program5.cpp
@@ -7,30 +7,73 @@
 #include <deque>
 
 using namespace std;
+//* Good start but not quite what we want.
+// template<typename T>
+// void read(istream& iss, T& t)
+// {
+//     if constexpr (is_same_v<string, T>)
+//     {
+//         getline(iss, t);
+//     }
+//     else
+//     {
+//         t = {};
+//     }
+// }
 
 template<typename T>
-void read(istream& iss, T& t)
+void read(istream& iss, T&& t);
+
+namespace details
 {
-    if constexpr (is_same_v<string, T>)
+    void read(istream& iss, string& str, int)
     {
-        getline(iss, t);
+        cout<< "2" << endl;
+
+        getline(iss, str);
     }
+
+    template<typename... Ts>
+    void read(istream& iss, tuple<Ts...>& tp, int)
+    {
+        cout<< "1" << endl;
+        (::read(iss, get<Ts>(tp)), ...);
+    }
+
+    template<typename T>
+    auto read(istream& iss, T&& t, int) -> decltype(iss >> t, declval<void>())
+    {
+        cout<< "4" << endl;
+        iss >> t;
+    }
+
+    template<typename T>
+    void read(istream& iss, T&& t, long long int)
+    {
+        t = {};
+    }
+}
+
+template<typename T>
+void read(istream& iss, T&& t)
+{
+    cout << "3" << endl;
+    details::read(iss, t, 0);
 }
 
 int main()
 {
-    
   {
-    // istringstream iss{""};
-    // vector<int> data{1, 2, 3};
-    // read(iss, data);
-    // assert(data.empty());
+    istringstream iss{""};
+    vector<int> data{1, 2, 3};
+    read(iss, data);
+    assert(data.empty());
   }
   {
-    // istringstream iss{"123"};
-    // int message{};
-    // read(iss, message);
-    // assert(message == 123);
+    istringstream iss{"123"};
+    int message{};
+    read(iss, message);
+    assert(message == 123);
   }
   {
     istringstream iss{"Hello Santa!"};
@@ -39,15 +82,15 @@ int main()
     assert(message == "Hello Santa!");
   }
   {
-    // istringstream iss{"Pie is good!\n 3.1415"};
-    // tuple<string, double> message{};
-    // read(iss, message);
-    // assert((message == make_tuple("Pie is good!", 3.1415)));
+    istringstream iss{"Pie is good!\n 3.1415"};
+    tuple<string, double> message{};
+    read(iss, message);
+    assert((message == make_tuple("Pie is good!", 3.1415)));
   }
   {
-    // istringstream iss{"1 2.3 abc"};
-    // tuple<int, double, char, string> message{};
-    // read(iss, message);
-    // assert((message == make_tuple(1, 2.3, 'a', "bc")));
+    istringstream iss{"1 2.3 abc"};
+    tuple<int, double, char, string> message{};
+    read(iss, message);
+    assert((message == make_tuple(1, 2.3, 'a', "bc")));
   }
 }
